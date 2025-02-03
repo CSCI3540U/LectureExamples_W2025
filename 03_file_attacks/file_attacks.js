a10b583cf21606096adc745896b227108a1cd099
randyfortier <randy.fortier@uoit.ca>
2025-02-03 14:24:48 -0500

    Added file upload demo and start of auth demo

--- 03_file_attacks/file_attacks.js
+++ 03_file_attacks/file_attacks.js
@@ -4,8 +4,18 @@ const port = 9000;
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
+const fileUpload = require('express-fileupload');
+const { execSync } = require('child_process');
 
 app.use(express.static('www'));
+app.use(fileUpload({
+    limits: { fileSize: 25 * 1024 * 1024 },
+    abortOnLimit: true,
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+    safeFileNames: true,
+    preserveExtension: true,
+}));
 
 app.get('/admin', (request, response) => {
     response.send('Admins welcome!');
@@ -55,6 +65,37 @@ app.get('/remote_file_inclusion', async (request, response) => {
         response.send(url_response.data);
     } catch (error) {
         response.status(500).send(`Error: ${error}`);
+    }
+});
+
+app.post('/file_upload', async (request, response) => {
+    const file = request.files.uploaded_image;
+    const desired_file_path = path.join(path.join(__dirname, 'uploads'), file.name);
+    // fix:
+    console.log('Checking file extension and MIME type.');
+    if (file.name.endsWith('.jpg') && file.mimetype === 'image/jpeg') {
+        console.log('Checking file contents with file command.');
+        const stdout = execSync(`file ${file.tempFilePath}`)
+        if (stdout.includes('JPEG')) {
+            // end fix
+            file.mv(desired_file_path, (error) => {
+                if (error) {
+                    response.send(`Error while moving the file: ${error}.`);
+                    response.end();
+                    return;
+                }
+        
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'text/html');
+                response.write(`File name: ${file.name}.`);
+                response.write(`File size: ${file.size}.`);
+                response.write(`File hash: ${file.md5}.`);
+                response.write(`MIME type: ${file.mimetype}.`);
+                response.write(`Temp path: ${file.tempFilePath}.`);
+                response.write(`Dest path: ${desired_file_path}.`);
+                response.end();
+            });        
+        }
     }
 });
 
---
+++ 04_broken_authentication/demo/broken_auth.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const port = 9000;
+
+let nextSessionId = 1;
+
+let sessionData = {
+    '1': {
+        email: 'admin@abc.com',
+        role: 'administrator'
+    }
+};
+
+let loginData = {
+    'admin@abc.com': 'v05!j_R',
+    'robert@abc.com': 'burt',
+    'sandra@abc.com': 'galdalf5',
+};
+
+function userExists(toFind) {
+    return Object.keys(loginData).includes(toFind);
+}
+
+function checkPassword(email, password) {
+    return loginData[email] === password;
+}
+
+app.use(express.static('static'));
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.get('/home', (request, response) => {
+    let sessionId = request.cookies['session_id'];
+    let email = sessionData[sessionId]['email'];
+    let role = sessionData[sessionId]['role'];
+    if (email) {
+        response.send(`Welcome, ${email}! Role: ${role}.`);
+        response.end();
+    } else {
+        response.redirect('/login');
+    }
+});
+
+app.get('/login', (request, response) => {
+    response.render('login', {
+        title: 'Please Sign In',
+        errorMessage: ''
+    });
+});
+
+app.post('/login', (request, response) => {
+    const email = request.body.email;
+    const password = request.body.password;
+
+    if (userExists(email)) {
+        if (checkPassword(email, password)) {
+            // login success
+            nextSessionId++;
+            const sessionId = nextSessionId;
+            response.cookie('session_id', sessionId);
+            sessionData[sessionId] = {
+                email: email,
+                role: 'user'
+            };
+            response.redirect('/home');
+        } else {
+            response.status(401).render('/login', {
+                title: 'Please Sign In',
+                errorMessage: 'Password does not match.'
+            });
+        }
+    } else {
+        response.status(401).render('/login', {
+            title: 'Please Sign In',
+            errorMessage: 'No such user exists.'
+        });
+    }
+});
+
+app.get('/logout', (request, response) => {
+    // request.session.email = '';
+    response.redirect('/login');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on pot ${port}.`)
+});
