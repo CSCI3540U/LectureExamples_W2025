8001f31c26cb16449c0a8f27d463726d58b85e46
randyfortier <randy.fortier@uoit.ca>
2025-02-24 21:01:42 -0500

    Added basic sqli

--- 04_broken_authentication/demo/broken_auth.js
+++ 04_broken_authentication/demo/broken_auth.js
@@ -3,30 +3,29 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const port = 9000;
 const session = require('express-session');
+const sqlite3 = require('sqlite3');
 
-// let nextSessionId = 1;
+const db = new sqlite3.Database('./auth.db');
 
-// let sessionData = {
-//     '1': {
-//         email: 'admin@abc.com',
-//         role: 'administrator'
-//     }
-// };
+let nextSessionId = 1;
+let sessionData = { };
 
-
-let loginData = {
-    'admin@abc.com': 'v05!j_R',
-    'robert@abc.com': 'burt',
-    'sandra@abc.com': 'galdalf5',
-};
-
-function userExists(toFind) {
-    return Object.keys(loginData).includes(toFind);
-}
-
-function checkPassword(email, password) {
-    return loginData[email] === password;
-}
+db.serialize(() => {
+    db.run(`create table if not exists Login(
+        email text primary key not null,
+        password text not null,
+        role text
+    )`);
+    db.run('delete from Login');
+    db.run(`insert into Login(email, password, role) values(?, ?, ?)`, ['robert@abc.com', 'burt', 'user']);
+    db.run(`insert into Login(email, password, role) values(?, ?, ?)`, ['bsmith@abc.com', 'barby', 'user']);
+    db.run(`insert into Login(email, password, role) values(?, ?, ?)`, ['akhan@abc.com', 'ahmed', 'user']);
+    db.run(`insert into Login(email, password, role) values(?, ?, ?)`, ['admin@abc.com', 'secret123', 'admin']);
+    console.log('Login table created.');
+    db.each('select * from Login', (error,row) => {
+        console.log(`   ${row['email']},${row['password']},${row['role']}`);
+    });
+});
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -44,21 +43,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 app.get('/home', (request, response) => {
-    // let sessionId = request.cookies['session_id'];
-    // console.log(`sessionId = ${sessionId}`);
-
-    // if (sessionId) {
-    //     let email = sessionData[sessionId]['email'];
-    //     let role = sessionData[sessionId]['role'];
-    //     if (email) {
-    //         response.send(`Welcome, ${email}! Role: ${role}.`);
-    //         response.end();
-    //         return;
-    //     }
-    // }
-    // fix:
-    if (request.session.email) {
-        response.send(`Welcome, ${request.session.email}! Role: ${request.session.role}.`);
+    const sessionId = request.cookies['session_id'];
+    // console.log(`sessionData = ${JSON.stringify(sessionData[sessionId])}`);
+    if (sessionData[sessionId]['email']) {
+        response.send(`Welcome, ${sessionData[sessionId]['email']}! Role: ${sessionData[sessionId]['role']}.<br /><a href="/login">Log In Again</a>`);
         response.end();
         return;
     }
@@ -76,30 +64,29 @@ app.post('/login', (request, response) => {
     const email = request.body.email;
     const password = request.body.password;
 
-    sleep(500).then(() => {
-        if (userExists(email)) {
-            if (checkPassword(email, password)) {
-                // login success
-                // nextSessionId++;
-                // const sessionId = nextSessionId;
-                // response.cookie('session_id', sessionId);
-                // sessionData[sessionId] = {
-                //     email: email,
-                //     role: 'user'
-                // };
-                // fix:
-                request.session['email'] = email;
-                request.session.role = 'user';
-                if (email === 'admin@abc.com') {
-                    request.session.role = 'admin';
-                }
-                response.redirect('/home');
-                return;
+    sleep(5 /*500*/).then(() => {
+        // db.all(`select email, role from Login where email = ? and password = ?`, [email, password], (error, rows) => {
+        db.all(`select email, role from Login where email = '${email}' and password = '${password}'`, (error, rows) => {
+            if ((error && error !== null) || rows.length == 0) {
+                console.log(`FAILED: Result of query: ${rows}, error: ${error}`);
+
+                // invalid login
+                response.render('login', {
+                    title: 'Login Page',
+                    errorMessage: error
+                });
+            } else {
+                let loginEmail = rows[0]['email'];
+                let role = rows[0]['role'];
+                console.log(`Login success: ${loginEmail}`);
+
+                // email and password are a match
+                response.cookie('session_id', `${nextSessionId}`);
+                sessionData[nextSessionId] = {email: loginEmail, role: role};
+                nextSessionId++;
+
+                response.redirect(`/profile/${loginEmail}`);
             }
-        }
-        response.status(401).render('login', {
-            title: 'Please Sign In',
-            errorMessage: 'Incorrect login.'
         });
     });
 });
@@ -111,16 +98,15 @@ app.get('/logout', (request, response) => {
 
 // access control
 
-app.get('/profile/:email', (request, response) => {
-    // const sessionId = request.cookies['session_id;'];
-    // console.log(`sessionId = ${sessionId}`);
-    // const email = sessionData[sessionId]['email'];
-    const email = request.session.email;
-    const role = request.session.role;
+app.get('/profile/:email', (request, response) => {    
+    const sessionId = request.cookies['session_id'];
+    const email = sessionData[sessionId]['email'];
+    const role = sessionData[sessionId]['role'];
+
     const emailParam = request.params['email'];
 
-    if (((email === emailParam) || (role === 'admin')) && (emailParam in loginData)) {
-        response.send(`Ciao ${email}!  Your role is ${role}`);
+    if ((email === emailParam) || (role === 'admin')) {
+        response.send(`Ciao ${email}!  Your role is ${role}<br /><a href="/login">Log In Again</a>`);
     } else {
         response.status(400).send('Cannot view profile.');
     }
